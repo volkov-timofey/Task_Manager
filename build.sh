f8f756fb12a6f9ffa2f118f873525d262bb920b6
Tim Volkov <volkov.timm@gmail.com>
2024-02-28 15:18:26 +0500

    deploy to render

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 make install
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+poetry run python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+poetry run python manage.py migrate
