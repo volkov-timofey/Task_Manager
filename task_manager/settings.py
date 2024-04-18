1c147595408f195ce565f818974a712c19f0c6f6
Tim Volkov <volkov.timm@gmail.com>
2024-04-18 16:31:29 +0500

    fix for tests

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -33,10 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
-
-if 'test' in sys.argv:
-    DEBUG = False
+DEBUG = os.getenv('DEBUG', False)
 
 ROLLBAR_TOKEN = os.getenv('ROLLBAR_TOKEN')
 
