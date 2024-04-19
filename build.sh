9e353c70a2c8013fe376c9ed3804287836b2c00c
Tim Volkov <volkov.timm@gmail.com>
2024-04-19 20:52:54 +0500

    setting render.com

--- build.sh
+++ build.sh
@@ -5,5 +5,9 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 make install
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+# Convert static asset files
+poetry run python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+poetry run python manage.py makemigrations users
+poetry run python manage.py migrate
